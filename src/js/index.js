20d6ce4e5a3f5e4c921d12b0e3d5c92c87e67aca
DmitryASJS <ovir@mail.ru>
2020-12-23 23:43:10 +0300

    add components to scss: header, toolbar, formula, table

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 'use strict';
 
-import './module/module.one.js';
 import '../css/index.scss';
+
+import './module/module.one.js';
+
 console.log('Точка входа');
+console.log('Второе сообщение с файла index.js');
 
